a8b46b040778624beb901cace2faac0e93cdd4ab
JuliaPark <pje106@gmail.com>
2022-10-27 21:49:45 -0700

    add migrations and models

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
-def create_app(test_config=None):
+def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
 
-    from .routes.planet import planet_bp
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    from app.models.planet import Planet
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    from.routes.planet import planet_bp
     app.register_blueprint(planet_bp)
     
     return app
--- app/routes/planet.py
+++ app/routes/planet.py
@@ -1,45 +1,51 @@
-from flask import Blueprint, jsonify
-
-class Planet:
-    def __init__(self, id, name, description):
-        self.id = id
-        self.name = name
-        self.description = description
-
-planets = [Planet(1, "Mercury", "the smallest planet in the Solar System"),
-            Planet(2, "Venus", "The second planet from the sun"),
-            Planet(3, "Earth", "The third planet from the sun")
-]
+from flask import Blueprint, jsonify, request
+from app import db
+from app.models.planet import Planet
 
 planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planet")
 
-@planet_bp.route("", methods = ["GET"])
-def get_all_planets():
-    response = []
-    for planet in planets:
-        planet_dict = {
-            "id" : planet.id,
-            "name" : planet.name,
-            "description" : planet.description
-        }
-        response.append(planet_dict)
-    return jsonify(response),200
+@planet_bp.route("", methods=["POST"])
+def add_planet():
+    request_body = request.get_json()
+    new_planet = Planet(
+        name = request_body["name"],
+        description = request_body["description"],
+        size = request_body["size"]
+    )
 
-@planet_bp.route("/<planet_id>", methods=["GET"])
-def get_one_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except ValueError:
-        response_string = f"Invalid planet_id: '{planet_id}' must be an integer"
-        return jsonify({"message": response_string}), 400
+    db.session.add(new_planet)
+    db.session.commit()
 
-    for planet in planets:
-        if planet.id == planet_id:
-            planet_dict = {
-                "id" : planet.id,
-                "name" : planet.name,
-                "description" : planet.description
-            }
-            return jsonify(planet_dict), 200
-    response_message = f"Cound not find planet with ID {planet_id}"
-    return jsonify({"message": response_message}), 404
+    return {"id": new_planet.id}, 201
+
+
+# @planet_bp.route("", methods = ["GET"])
+# def get_all_planets():
+#     response = []
+#     for planet in planets:
+#         planet_dict = {
+#             "id" : planet.id,
+#             "name" : planet.name,
+#             "description" : planet.description
+#         }
+#         response.append(planet_dict)
+#     return jsonify(response),200
+
+# @planet_bp.route("/<planet_id>", methods=["GET"])
+# def get_one_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except ValueError:
+#         response_string = f"Invalid planet_id: '{planet_id}' must be an integer"
+#         return jsonify({"message": response_string}), 400
+
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             planet_dict = {
+#                 "id" : planet.id,
+#                 "name" : planet.name,
+#                 "description" : planet.description
+#             }
+#             return jsonify(planet_dict), 200
+#     response_message = f"Cound not find planet with ID {planet_id}"
+#     return jsonify({"message": response_message}), 404
